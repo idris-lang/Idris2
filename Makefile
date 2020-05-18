b69068f4ff1b1dd33a7bbf876d5c032d229f3fe2
Edwin Brady <ecb10@st-andrews.ac.uk>
2020-05-18 20:33:38 +0100

    Update bootstrap scripts

--- Makefile
+++ Makefile
@@ -96,4 +96,4 @@ install-libs: libs
 	${MAKE} -C libs/contrib install IDRIS2=../../${TARGET} IDRIS2_PATH=${IDRIS2_BOOT_PATH}
 
 init-bootstrap:
-	./init-bootstrap ${PREFIX}
+	./bootstrap.sh ${SCHEME} ${PREFIX}
--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,8 @@ cp support/c/$IDRIS2_SUPPORT bootstrap/idris2-boot_app
 cd bootstrap
 
 sed s/libidris2_support.so/$IDRIS2_SUPPORT/g idris2sh_app/idris2sh.ss > idris2sh_app/idris2-boot.ss
-${SCHEME} --script compile.ss
+sed -i "s|__PREFIX__|$2|g" idris2sh_app/idris2-boot.ss
+$1 --script compile.ss
 
 mkdir -p ../build/exec
 mkdir -p ../build/exec/idris2sh_app
