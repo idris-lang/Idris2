0b32bf8a2fb7ace1d48681a00445aaf446b3bc0b
Markus Pfeiffer <markus.pfeiffer@morphism.de>
2020-07-06 13:56:36 +0100

    Fix forward declaration of idris_closeDir

--- support/c/idris_directory.h
+++ support/c/idris_directory.h
@@ -5,7 +5,7 @@ char* idris2_currentDirectory();
 int idris2_changeDir(char* dir);
 int idris2_createDir(char* dir);
 void* idris2_openDir(char* dir);
-void idris2_closeDIr(void* d);
+void idris2_closeDir(void* d);
 int idris2_removeDir(char* path);
 char* idris2_nextDirEntry(void* d);
 
