21524da0a0c4f039388f24cd522cad7cdf7cbe4c
Giuseppe Lomurno <lomurno.giuseppe97@gmail.com>
2020-08-19 11:27:26 +0200

    Windows color fix

--- support/c/idris_term.c
+++ support/c/idris_term.c
@@ -2,6 +2,18 @@
 
 #include <windows.h>
 
+#ifndef ENABLE_VIRTUAL_TERMINAL_PROCESSING
+#define ENABLE_VIRTUAL_TERMINAL_PROCESSING  0x0004
+#endif
+
+void idris2_setupTerm() {
+    HANDLE stdout_handle = GetStdHandle(STD_OUTPUT_HANDLE);
+    DWORD outmode = 0;
+    GetConsoleMode(stdout_handle, &outmode);
+    outmode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+    SetConsoleMode(stdout_handle, outmode);
+}
+
 int idris2_getTermCols() {
     CONSOLE_SCREEN_BUFFER_INFO csbi;
     GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
@@ -17,6 +29,10 @@ int idris2_getTermLines() {
 #else
 
 #include <sys/ioctl.h>
+
+void idris2_setupTerm() {
+    // NOTE: Currently not needed for non windows systems
+}
 
 int idris2_getTermCols() {
     struct winsize ts;
