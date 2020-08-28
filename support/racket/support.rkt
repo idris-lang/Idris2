0c4a1f8fa6c4e9a8c8824634631f4974906b17e7
foss-mc <69294560+foss-mc@users.noreply.github.com>
2020-08-28 19:02:04 +0800

    Improve Racket supporting code

--- support/racket/support.rkt
+++ support/racket/support.rkt
@@ -1,8 +1,8 @@
 (define (blodwen-os)
-  (cond
-    [(eq? (system-type 'os) 'unix) "unix"]
-    [(eq? (system-type 'os) 'osx) "darwin"]
-    [(eq? (system-type 'os) 'windows) "windows"]
+  (case (system-type 'os)
+    [(unix) "unix"]
+    [(osx) "darwin"]
+    [(windows) "windows"]
     [else "unknown"]))
 
 (define blodwen-read-args (lambda (desc)
@@ -59,8 +59,8 @@
   (if (= (vector-ref xs 0) 0)
     '()
     (cons (vector-ref xs 1) (from-idris-list (vector-ref xs 2)))))
-(define (string-concat xs) (apply string-append (from-idris-list xs)))
-(define (string-pack xs) (apply string (from-idris-list xs)))
+(define (string-concat xs) (string-join (from-idris-list xs) ""))
+(define (string-pack xs) (list->string (from-idris-list xs)))
 (define string-cons (lambda (x y) (string-append (string x) y)))
 (define get-tag (lambda (x) (vector-ref x 0)))
 (define string-reverse (lambda (x)
@@ -92,7 +92,7 @@
             (if (eof-object? str)
                 ""
                 str))
-        void))
+        (void)))
 
 (define (blodwen-get-char p)
     (if (port? p)
@@ -100,7 +100,7 @@
             (if (eof-object? chr)
                 #\nul
                 chr))
-        void))
+        (void)))
 
 ;; Buffers
 
