fd15a221da0be2f55c8dfb422ed97c1acb092010
Mathew Polzin <matt.polzin@gmail.com>
2021-02-15 20:07:22 -0800

    more JS linter fixes

--- support/js/support_system_file.js
+++ support/js/support_system_file.js
@@ -1,8 +1,8 @@
-const support_system_file_fs = require("fs")
+const support_system_file_fs = require('fs')
 
 // like `readLine` without the overhaead of copying characters.
 // returns int (success 0, failure -1) to align with the C counterpart.
-function support_system_file_seekLine(file_ptr) {
+function support_system_file_seekLine (file_ptr) {
   const LF = 0x0a
   const readBuf = Buffer.alloc(1)
   let lineEnd = file_ptr.buffer.indexOf(LF)
@@ -20,7 +20,7 @@ function support_system_file_seekLine(file_ptr) {
   return 0n
 }
 
-function support_system_file_readLine(file_ptr) {
+function support_system_file_readLine (file_ptr) {
   const LF = 0x0a
   const readBuf = Buffer.alloc(1)
   let lineEnd = file_ptr.buffer.indexOf(LF)
@@ -40,25 +40,25 @@ function support_system_file_readLine(file_ptr) {
   return line
 }
 
-function support_system_file_getStr() {
-  return support_system_file_readLine({fd:0, buffer: Buffer.alloc(0), name:'<stdin>', eof: false})
+function support_system_file_getStr () {
+  return support_system_file_readLine({ fd: 0, buffer: Buffer.alloc(0), name: '<stdin>', eof: false })
 }
 
-function support_system_file_openFile(n,m) {
-  try{
+function support_system_file_openFile (n, m) {
+  try {
     const fd = support_system_file_fs.openSync(n, m)
-    return {fd: fd, buffer: Buffer.alloc(0), name:n, eof: false}
-  } catch(e) {
+    return { fd: fd, buffer: Buffer.alloc(0), name: n, eof: false }
+  } catch (e) {
     process.__lasterr = e
     return null
   }
 }
 
-function support_system_file_chmod(filename, mode) {
+function support_system_file_chmod (filename, mode) {
   try {
     support_system_file_fs.chmodSync(filename, Number(mode))
     return 0n
-  } catch(e) {
+  } catch (e) {
     process.__lasterr = e
     return 1n
   }
