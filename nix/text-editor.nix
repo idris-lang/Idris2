a07c14f5ed510126ba16b8a7b0abcf0a6f769e8a
Guilherme <guilhermehas@hotmail.com>
2021-03-03 16:10:08 -0300

    added emacs wrapper

--- nix/text-editor.nix
+++ nix/text-editor.nix
@@ -2,19 +2,22 @@
 , idris-emacs-src
 }: with pkgs;
 let
-  makeEmacsWrapper = name: idris: writeShellScriptBin name ''
-    ${idris}/bin/emacs --eval "(progn (require 'idris2-mode))" $@
+  init-file = ./init.el;
+  makeEmacsWrapper = name: my-emacs: init: writeShellScriptBin name ''
+    ${my-emacs}/bin/emacs -q -l ${init-file} $@
   '';
 in rec {
   idris2-mode = emacsPackages.melpaBuild {
     pname = "idris2-mode";
     src = idris-emacs-src;
-    packageRequires = with pkgs.emacsPackages.melpaPackages; [ prop-menu ];
+    packageRequires = with pkgs.emacsPackages.melpaPackages; [
+      prop-menu
+    ];
     version = "1";
     recipe = pkgs.writeText "recipe" ''
       (idris2-mode :repo "redfish64/idris2-mode" :fetcher github)
     '';
   };
   idris-emacs = emacsWithPackages [ idris2-mode ];
-  emacs-dev = makeEmacsWrapper "emacs-dev" idris-emacs;
+  emacs-dev = makeEmacsWrapper "emacs-dev" idris-emacs init-file;
 }
