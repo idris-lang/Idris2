da191d9a65fcf4af55cb009d569133c29c530728
Guilherme <guilhermehas@hotmail.com>
2021-03-08 02:45:33 -0300

    added test with importing library

--- nix/test.nix
+++ nix/test.nix
@@ -16,14 +16,29 @@ let
             touch "$out"
           '') tests;
      in testDrvs;
-  self = import ./templates/pkg/flake.nix;
-  template = self.outputs { inherit self nixpkgs idris flake-utils; };
-  templateBuild = template.packages.${system}.build;
+  createTemplate = flake: inputs: type: let
+    self = import flake;
+    template = self.outputs ({ inherit self nixpkgs idris flake-utils; } // inputs);
+    templateBuild = template.packages.${system}.${type};
+    in templateBuild;
+
+  templateBuildDefault = createTemplate ./templates/pkg/flake.nix {} "build";
+  templateBuildDefaultLibrary = createTemplate ./templates/pkg/flake.nix {} "installLibrary";
+  templateBuildWithDeps = createTemplate ./templates/pkgWithDeps/flake.nix
+    { pkg = templateBuildDefaultLibrary; } "build";
+
   testsTemplate = {
     checkFoo = ''
-      ${templateBuild}/bin/runMyPkg \
+      ${templateBuildDefault}/bin/runMyPkg \
         | grep "Foo"
     '';
   };
+  testsTemplateWithDeps = {
+    checkBar = ''
+      ${templateBuildWithDeps}/bin/runMyPkg2 \
+        | grep "Bar"
+    '';
+  };
 in
-withTests testsTemplate templateBuild
+withTests testsTemplate templateBuildDefault
+// withTests testsTemplateWithDeps templateBuildWithDeps
