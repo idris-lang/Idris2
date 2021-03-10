d821c793290f84bf60f4e25f992a7dbc7ccb655f
Guilherme <guilhermehas@hotmail.com>
2021-03-10 00:08:48 -0300

    include test in includedLibs

--- nix/package.nix
+++ nix/package.nix
@@ -33,7 +33,7 @@ stdenv.mkDerivation rec {
   # TODO: Move this into its own derivation, such that this can be changed
   #       without having to recompile idris2 every time.
   postInstall = let
-    includedLibs = [ "base" "contrib" "network" "prelude" ];
+    includedLibs = [ "base" "contrib" "network" "prelude" "test" ];
     name = "${pname}-${version}";
     packagePaths = builtins.map (l: "$out/${name}/${l}-${version}") includedLibs;
     additionalIdris2Paths = builtins.concatStringsSep ":" packagePaths;
