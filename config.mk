722bab81100e3272971cc5f0141e8a2ef8aa451d
I B 3 <ME@IB3.IO>
2021-06-18 11:06:45 +0100

    Stop pointing at the Void

--- config.mk
+++ config.mk
@@ -1,6 +1,6 @@
 ##### Options which a user might set before building go here #####
 
-# Where to install idris2 binaries and libraries (must not be a relative path)
+# Where to install idris2 binaries and libraries (must be an absolute path)
 PREFIX ?= $(HOME)/.idris2
 
 # For Windows targets. Set to 1 to support Windows 7.
