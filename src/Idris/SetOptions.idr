ska80/Idris2-SH

[ cleanup ] Add idents to some underindented stuff

src/Idris/SetOptions.idr
@@ -230,14 +230,14 @@ opts x _ = pure $ if (x `elem` optionFlags)
 -- bash autocompletion script using the given function name
 completionScript : (fun : String) -> String
 completionScript fun = let fun' = "_" ++ fun in #"""
-\#{ fun' }()
-{
-  ED=$([ -z $2 ] && echo "--" || echo $2)
-  COMPREPLY=($(idris2 --bash-completion $ED $3))
-}
+  \#{ fun' }()
+  {
+    ED=$([ -z $2 ] && echo "--" || echo $2)
+    COMPREPLY=($(idris2 --bash-completion $ED $3))
+  }
 
-complete -F \#{ fun' } -o dirnames idris2
-"""#
+  complete -F \#{ fun' } -o dirnames idris2
+  """#
 
 --------------------------------------------------------------------------------
 --          Processing Options

idris-lang/Idris2

src/Idris/SetOptions.idr
@@ -249,14 +249,14 @@ opts x _ = pure $ if (x `elem` optionFlags)
 -- bash autocompletion script using the given function name
 completionScript : (fun : String) -> String
 completionScript fun = let fun' = "_" ++ fun in #"""
-\#{ fun' }()
-{
-  ED=$([ -z $2 ] && echo "--" || echo $2)
-  COMPREPLY=($(idris2 --bash-completion $ED $3))
-}
+  \#{ fun' }()
+  {
+    ED=$([ -z $2 ] && echo "--" || echo $2)
+    COMPREPLY=($(idris2 --bash-completion $ED $3))
+  }
 
-complete -F \#{ fun' } -o default idris2
-"""#
+  complete -F \#{ fun' } -o default idris2
+  """#
 
 --------------------------------------------------------------------------------
 --          Processing Options