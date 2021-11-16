64ba9e45d3ba79fcfe9452ed713818e21f850103
Denis Buzdalov <public@buzden.ru>
2021-11-17 01:13:20 +0300

    [ fix ] Fix returned status of the `system` function

--- support/c/idris_system.c
+++ support/c/idris_system.c
@@ -2,5 +2,10 @@
 #include "idris_system.h"
 
 int idris2_system(const char* command) {
-    return system(command);
+    int status = system(command);
+    #ifdef _WIN32
+      return status;
+    #else
+      return WIFEXITED(status) ? WEXITSTATUS(status) : -1;
+    #endif
 }
