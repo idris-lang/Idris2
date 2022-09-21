c38e6bcfa9a5378c14ba12d1f8771fe0ece8b2f2
Guillaume Allais <guillaume.allais@ens-lyon.org>
2022-09-21 11:13:15 +0100

    [ lint ] once and for all

--- support/refc/buffer.c
+++ support/refc/buffer.c
@@ -1,118 +1,115 @@
 #include "buffer.h"
 #include "refc_util.h"
-#include <sys/stat.h>
 #include <string.h>
+#include <sys/stat.h>
 
-void* newBuffer(int bytes) {
-    size_t size = sizeof(Buffer) + bytes*sizeof(uint8_t);
+void *newBuffer(int bytes) {
+  size_t size = sizeof(Buffer) + bytes * sizeof(uint8_t);
 
-    Buffer* buf = malloc(size);
-    if (buf == NULL) {
-        return NULL;
-    }
+  Buffer *buf = malloc(size);
+  if (buf == NULL) {
+    return NULL;
+  }
 
-    buf->size = bytes;
-    memset(buf->data, 0, bytes);
+  buf->size = bytes;
+  memset(buf->data, 0, bytes);
 
-    return (void*)buf;
+  return (void *)buf;
 }
 
-static void assert_valid_range(Buffer* buf, int64_t offset, int64_t len) {
-    IDRIS2_REFC_VERIFY(offset >= 0, "offset (%lld) < 0", (long long) offset);
-    IDRIS2_REFC_VERIFY(len >= 0, "len (%lld) < 0", (long long) offset);
-    IDRIS2_REFC_VERIFY(offset + len <= buf->size,
-        "offset (%lld) + len (%lld) > buf.size (%lld)",
-        (long long) offset, (long long) len, (long long) buf->size);
+static void assert_valid_range(Buffer *buf, int64_t offset, int64_t len) {
+  IDRIS2_REFC_VERIFY(offset >= 0, "offset (%lld) < 0", (long long)offset);
+  IDRIS2_REFC_VERIFY(len >= 0, "len (%lld) < 0", (long long)offset);
+  IDRIS2_REFC_VERIFY(offset + len <= buf->size,
+                     "offset (%lld) + len (%lld) > buf.size (%lld)",
+                     (long long)offset, (long long)len, (long long)buf->size);
 }
 
-void copyBuffer(void* from, int from_offset, int len,
-                void* to, int to_offset) {
-    Buffer* bfrom = from;
-    Buffer* bto = to;
+void copyBuffer(void *from, int from_offset, int len, void *to, int to_offset) {
+  Buffer *bfrom = from;
+  Buffer *bto = to;
 
-    assert_valid_range(bfrom, from_offset, len);
-    assert_valid_range(bto, to_offset, len);
+  assert_valid_range(bfrom, from_offset, len);
+  assert_valid_range(bto, to_offset, len);
 
-    memcpy(bto->data + to_offset, bfrom->data + from_offset, len);
+  memcpy(bto->data + to_offset, bfrom->data + from_offset, len);
 }
 
-int getBufferSize(void* buffer) {
-    return ((Buffer*)buffer)->size;
+int getBufferSize(void *buffer) { return ((Buffer *)buffer)->size; }
+
+void setBufferByte(void *buffer, int loc, int byte) {
+  Buffer *b = buffer;
+
+  assert_valid_range(buffer, loc, 1);
+
+  b->data[loc] = byte;
 }
 
-void setBufferByte(void* buffer, int loc, int byte) {
-    Buffer* b = buffer;
-
-    assert_valid_range(buffer, loc, 1);
-
-    b->data[loc] = byte;
+void setBufferInt(void *buffer, int loc, int64_t val) {
+  Buffer *b = buffer;
+  assert_valid_range(b, loc, 8);
+  b->data[loc] = val & 0xff;
+  b->data[loc + 1] = (val >> 8) & 0xff;
+  b->data[loc + 2] = (val >> 16) & 0xff;
+  b->data[loc + 3] = (val >> 24) & 0xff;
+  b->data[loc + 4] = (val >> 32) & 0xff;
+  b->data[loc + 5] = (val >> 40) & 0xff;
+  b->data[loc + 6] = (val >> 48) & 0xff;
+  b->data[loc + 7] = (val >> 56) & 0xff;
 }
 
-void setBufferInt(void* buffer, int loc, int64_t val) {
-    Buffer* b = buffer;
-    assert_valid_range(b, loc, 8);
-    b->data[loc  ] =  val        & 0xff;
-    b->data[loc+1] = (val >>  8) & 0xff;
-    b->data[loc+2] = (val >> 16) & 0xff;
-    b->data[loc+3] = (val >> 24) & 0xff;
-    b->data[loc+4] = (val >> 32) & 0xff;
-    b->data[loc+5] = (val >> 40) & 0xff;
-    b->data[loc+6] = (val >> 48) & 0xff;
-    b->data[loc+7] = (val >> 56) & 0xff;
+void setBufferDouble(void *buffer, int loc, double val) {
+  Buffer *b = buffer;
+  assert_valid_range(b, loc, sizeof(double));
+  unsigned char *c = (unsigned char *)(&val);
+  int i;
+  for (i = 0; i < sizeof(double); ++i) {
+    b->data[loc + i] = c[i];
+  }
 }
 
-void setBufferDouble(void* buffer, int loc, double val) {
-    Buffer* b = buffer;
-    assert_valid_range(b, loc, sizeof(double));
-    unsigned char* c = (unsigned char*)(& val);
-    int i;
-    for (i = 0; i < sizeof(double); ++i) {
-        b->data[loc+i] = c[i];
-    }
+void setBufferString(void *buffer, int loc, char *str) {
+  Buffer *b = buffer;
+  size_t len = strlen(str);
+  assert_valid_range(b, loc, len);
+  memcpy((b->data) + loc, str, len);
 }
 
-void setBufferString(void* buffer, int loc, char* str) {
-    Buffer* b = buffer;
-    size_t len = strlen(str);
-    assert_valid_range(b, loc, len);
-    memcpy((b->data)+loc, str, len);
+uint8_t getBufferByte(void *buffer, int loc) {
+  Buffer *b = buffer;
+  assert_valid_range(b, loc, 1);
+  return b->data[loc];
 }
 
-uint8_t getBufferByte(void* buffer, int loc) {
-    Buffer* b = buffer;
-    assert_valid_range(b, loc, 1);
-    return b->data[loc];
+int64_t getBufferInt(void *buffer, int loc) {
+  Buffer *b = buffer;
+  assert_valid_range(b, loc, 8);
+  int64_t result = 0;
+  for (size_t i = 0; i < 8; i++) {
+    result |= (uint64_t)(uint8_t)b->data[loc + i] << (8 * i);
+  }
+  return result;
 }
 
-int64_t getBufferInt(void* buffer, int loc) {
-    Buffer* b = buffer;
-    assert_valid_range(b, loc, 8);
-    int64_t result = 0;
-    for (size_t i=0; i<8; i++) {
-        result |= (uint64_t)(uint8_t)b->data[loc + i] << (8 * i);
-    }
-    return result;
+double getBufferDouble(void *buffer, int loc) {
+  Buffer *b = buffer;
+  assert_valid_range(b, loc, sizeof(double));
+  double d;
+  // I am even less proud of this
+  unsigned char *c = (unsigned char *)(&d);
+  int i;
+  for (i = 0; i < sizeof(double); ++i) {
+    c[i] = b->data[loc + i];
+  }
+  return d;
 }
 
-double getBufferDouble(void* buffer, int loc) {
-    Buffer* b = buffer;
-    assert_valid_range(b, loc, sizeof(double));
-    double d;
-    // I am even less proud of this
-    unsigned char *c = (unsigned char*)(& d);
-    int i;
-    for (i = 0; i < sizeof(double); ++i) {
-        c[i] = b->data[loc+i];
-    }
-    return d;
-}
-
-char* getBufferString(void* buffer, int loc, int len) {
-    Buffer* b = buffer;
-    assert_valid_range(b, loc, len);
-    char * s = (char*)(b->data + loc);
-    char * rs = malloc(len + 1);
-    strncpy(rs, s, len);
-    rs[len] = '\0';
-    return rs;
+char *getBufferString(void *buffer, int loc, int len) {
+  Buffer *b = buffer;
+  assert_valid_range(b, loc, len);
+  char *s = (char *)(b->data + loc);
+  char *rs = malloc(len + 1);
+  strncpy(rs, s, len);
+  rs[len] = '\0';
+  return rs;
 }
--- support/refc/buffer.h
+++ support/refc/buffer.h
@@ -1,27 +1,26 @@
 #pragma once
 
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 
 typedef struct {
-    int size;
-    char data[];
+  int size;
+  char data[];
 } Buffer;
 
-void* newBuffer(int bytes);
+void *newBuffer(int bytes);
 
-int getBufferSize(void* buffer);
+int getBufferSize(void *buffer);
 
-void setBufferByte(void* buffer, int loc, int byte);
-void setBufferInt(void* buffer, int loc, int64_t val);
-void setBufferDouble(void* buffer, int loc, double val);
-void setBufferString(void* buffer, int loc, char* str);
+void setBufferByte(void *buffer, int loc, int byte);
+void setBufferInt(void *buffer, int loc, int64_t val);
+void setBufferDouble(void *buffer, int loc, double val);
+void setBufferString(void *buffer, int loc, char *str);
 
-void copyBuffer(void* from, int start, int len,
-                void* to, int loc);
+void copyBuffer(void *from, int start, int len, void *to, int loc);
 
-uint8_t getBufferByte(void* buffer, int loc);
-int64_t getBufferInt(void* buffer, int loc);
-double getBufferDouble(void* buffer, int loc);
-char* getBufferString(void* buffer, int loc, int len);
+uint8_t getBufferByte(void *buffer, int loc);
+int64_t getBufferInt(void *buffer, int loc);
+double getBufferDouble(void *buffer, int loc);
+char *getBufferString(void *buffer, int loc, int len);
