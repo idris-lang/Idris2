bfa8ebf72f34fe7f60949ecc79448bbe0495d484
Guillaume Allais <guillaume.allais@ens-lyon.org>
2022-09-21 12:17:47 +0100

    [ fix ] formatting

--- support/refc/buffer.c
+++ support/refc/buffer.c
@@ -1,92 +1,92 @@
 #include "buffer.h"
 #include "refc_util.h"
-#include <sys/stat.h>
 #include <string.h>
+#include <sys/stat.h>
 
-void* newBuffer(int bytes) {
-    size_t size = sizeof(Buffer) + bytes*sizeof(uint8_t);
+void *newBuffer(int bytes) {
+  size_t size = sizeof(Buffer) + bytes * sizeof(uint8_t);
 
-    Buffer* buf = malloc(size);
-    if (buf == NULL) {
-        return NULL;
-    }
+  Buffer *buf = malloc(size);
+  if (buf == NULL) {
+    return NULL;
+  }
 
-    buf->size = bytes;
-    memset(buf->data, 0, bytes);
+  buf->size = bytes;
+  memset(buf->data, 0, bytes);
 
-    return (void*)buf;
+  return (void *)buf;
 }
 
-static void assert_valid_range(Buffer* buf, int64_t offset, int64_t len) {
-    IDRIS2_REFC_VERIFY(offset >= 0, "offset (%lld) < 0", (long long) offset);
-    IDRIS2_REFC_VERIFY(len >= 0, "len (%lld) < 0", (long long) offset);
-    IDRIS2_REFC_VERIFY(offset + len <= buf->size,
-        "offset (%lld) + len (%lld) > buf.size (%lld)",
-        (long long) offset, (long long) len, (long long) buf->size);
+static void assert_valid_range(Buffer *buf, int64_t offset, int64_t len) {
+  IDRIS2_REFC_VERIFY(offset >= 0, "offset (%lld) < 0", (long long)offset);
+  IDRIS2_REFC_VERIFY(len >= 0, "len (%lld) < 0", (long long)offset);
+  IDRIS2_REFC_VERIFY(offset + len <= buf->size,
+                     "offset (%lld) + len (%lld) > buf.size (%lld)",
+                     (long long)offset, (long long)len, (long long)buf->size);
 }
 
-void copyBuffer(void* from, int from_offset, int len,
-                void* to, int to_offset) {
-    Buffer* bfrom = from;
-    Buffer* bto = to;
+void copyBuffer(void *from, int from_offset, int len, void *to, int to_offset) {
+  Buffer *bfrom = from;
+  Buffer *bto = to;
 
-    assert_valid_range(bfrom, from_offset, len);
-    assert_valid_range(bto, to_offset, len);
+  assert_valid_range(bfrom, from_offset, len);
+  assert_valid_range(bto, to_offset, len);
 
-    memcpy(bto->data + to_offset, bfrom->data + from_offset, len);
+  memcpy(bto->data + to_offset, bfrom->data + from_offset, len);
 }
 
-int getBufferSize(void* buffer) {
-    return ((Buffer*)buffer)->size;
-}
+int getBufferSize(void *buffer) { return ((Buffer *)buffer)->size; }
 
-
-
-void setBufferWordLE(void* b, int loc, uint64_t val, size_t len) {
-  assert_valid_range((Buffer*)b, loc, len);
+void setBufferWordLE(void *b, int loc, uint64_t val, size_t len) {
+  assert_valid_range((Buffer *)b, loc, len);
   while (len--) {
-    ((Buffer*)b)->data[loc++] = (uint8_t)val;
+    ((Buffer *)b)->data[loc++] = (uint8_t)val;
     val >>= 8;
   }
 }
 
-uint64_t getBufferWordLE(void* b, int loc, size_t len) {
-  assert_valid_range((Buffer*)b, loc, len);
+uint64_t getBufferWordLE(void *b, int loc, size_t len) {
+  assert_valid_range((Buffer *)b, loc, len);
   uint64_t r = 0;
   loc += len;
   while (len--) {
     r <<= 8;
-    r += (uint8_t)(((Buffer*)b)->data[--loc]);
+    r += (uint8_t)(((Buffer *)b)->data[--loc]);
   }
   return r;
 }
 
-
-void setBufferDouble(void* buffer, int loc, double val) {
-    union { double d; uint64_t i; } tmp;
-    tmp.d = val;
-    setBufferWordLE(buffer, loc, tmp.i, 8);
+void setBufferDouble(void *buffer, int loc, double val) {
+  union {
+    double d;
+    uint64_t i;
+  } tmp;
+  tmp.d = val;
+  setBufferWordLE(buffer, loc, tmp.i, 8);
 }
 
-void setBufferString(void* buffer, int loc, char* str) {
-    Buffer* b = buffer;
-    size_t len = strlen(str);
-    assert_valid_range(b, loc, len);
-    memcpy((b->data)+loc, str, len);
+void setBufferString(void *buffer, int loc, char *str) {
+  Buffer *b = buffer;
+  size_t len = strlen(str);
+  assert_valid_range(b, loc, len);
+  memcpy((b->data) + loc, str, len);
 }
 
-double getBufferDouble(void* buffer, int loc) {
-    union { double d; uint64_t i; } tmp;
-    tmp.i = getBufferWordLE(buffer, loc, 8);
-    return tmp.d;
+double getBufferDouble(void *buffer, int loc) {
+  union {
+    double d;
+    uint64_t i;
+  } tmp;
+  tmp.i = getBufferWordLE(buffer, loc, 8);
+  return tmp.d;
 }
 
-char* getBufferString(void* buffer, int loc, int len) {
-    Buffer* b = buffer;
-    assert_valid_range(b, loc, len);
-    char * s = (char*)(b->data + loc);
-    char * rs = malloc(len + 1);
-    strncpy(rs, s, len);
-    rs[len] = '\0';
-    return rs;
+char *getBufferString(void *buffer, int loc, int len) {
+  Buffer *b = buffer;
+  assert_valid_range(b, loc, len);
+  char *s = (char *)(b->data + loc);
+  char *rs = malloc(len + 1);
+  strncpy(rs, s, len);
+  rs[len] = '\0';
+  return rs;
 }
--- support/refc/buffer.h
+++ support/refc/buffer.h
@@ -1,45 +1,67 @@
 #pragma once
 
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 
 typedef struct {
-    int size;
-    char data[];
+  int size;
+  char data[];
 } Buffer;
 
-void* newBuffer(int bytes);
+void *newBuffer(int bytes);
 
-int getBufferSize(void* buffer);
+int getBufferSize(void *buffer);
 
-void setBufferWordLE(void* buffer, int loc, uint64_t val,  size_t len);
-#define setBufferWord8(b, l, v)          do { setBufferWordLE(b, l, (uint64_t)v, 1); } while (0)
-#define setBufferWord16LE(b, l, v)       do { setBufferWordLE(b, l, (uint64_t)v, 2); } while (0)
-#define setBufferWord32LE(b, l, v)       do { setBufferWordLE(b, l, (uint64_t)v, 4); } while (0)
-#define setBufferWord64LE(b, l, v)       do { setBufferWordLE(b, l, (uint64_t)v, 8); } while (0)
+void setBufferWordLE(void *buffer, int loc, uint64_t val, size_t len);
+#define setBufferWord8(b, l, v)                                                \
+  do {                                                                         \
+    setBufferWordLE(b, l, (uint64_t)v, 1);                                     \
+  } while (0)
+#define setBufferWord16LE(b, l, v)                                             \
+  do {                                                                         \
+    setBufferWordLE(b, l, (uint64_t)v, 2);                                     \
+  } while (0)
+#define setBufferWord32LE(b, l, v)                                             \
+  do {                                                                         \
+    setBufferWordLE(b, l, (uint64_t)v, 4);                                     \
+  } while (0)
+#define setBufferWord64LE(b, l, v)                                             \
+  do {                                                                         \
+    setBufferWordLE(b, l, (uint64_t)v, 8);                                     \
+  } while (0)
 
-#define setBufferByte(b, l, v)           do { setBufferWordLE(b, l, (uint64_t)v, 1); } while (0)
-#define setBufferInt16LE(b, l, v)        do { setBufferWordLE(b, l, (uint64_t)v, 2); } while (0)
-#define setBufferInt32LE(b, l, v)        do { setBufferWordLE(b, l, (uint64_t)v, 4); } while (0)
-#define setBufferInt64LE(b, l, v)        do { setBufferWordLE(b, l, (uint64_t)v, 8); } while (0)
+#define setBufferByte(b, l, v)                                                 \
+  do {                                                                         \
+    setBufferWordLE(b, l, (uint64_t)v, 1);                                     \
+  } while (0)
+#define setBufferInt16LE(b, l, v)                                              \
+  do {                                                                         \
+    setBufferWordLE(b, l, (uint64_t)v, 2);                                     \
+  } while (0)
+#define setBufferInt32LE(b, l, v)                                              \
+  do {                                                                         \
+    setBufferWordLE(b, l, (uint64_t)v, 4);                                     \
+  } while (0)
+#define setBufferInt64LE(b, l, v)                                              \
+  do {                                                                         \
+    setBufferWordLE(b, l, (uint64_t)v, 8);                                     \
+  } while (0)
 
-void setBufferDouble(void* buffer, int loc, double val);
-void setBufferString(void* buffer, int loc, char* str);
+void setBufferDouble(void *buffer, int loc, double val);
+void setBufferString(void *buffer, int loc, char *str);
 
-void copyBuffer(void* from, int start, int len,
-                void* to, int loc);
+void copyBuffer(void *from, int start, int len, void *to, int loc);
 
-uint64_t getBufferWordLE(void* buffer, int loc, size_t len);
-#define getBufferWord8(b, l)             ((uint8_t)getBufferWordLE(b, l, 1))
-#define getBufferWord16LE(b, l)          ((uint16_t)getBufferWordLE(b, l, 2))
-#define getBufferWord32LE(b, l)          ((uint32_t)getBufferWordLE(b, l, 4))
-#define getBufferWord64LE(b, l)          ((uint64_t)getBufferWordLE(b, l, 8))
+uint64_t getBufferWordLE(void *buffer, int loc, size_t len);
+#define getBufferWord8(b, l) ((uint8_t)getBufferWordLE(b, l, 1))
+#define getBufferWord16LE(b, l) ((uint16_t)getBufferWordLE(b, l, 2))
+#define getBufferWord32LE(b, l) ((uint32_t)getBufferWordLE(b, l, 4))
+#define getBufferWord64LE(b, l) ((uint64_t)getBufferWordLE(b, l, 8))
 
-#define getBufferByte(b, l)              ((int64_t)getBufferWordLE(b, l, 1))
-#define getBufferInt16LE(b, l)           ((int64_t)getBufferWordLE(b, l, 2))
-#define getBufferInt32LE(b, l)           ((int64_t)getBufferWordLE(b, l, 4))
-#define getBufferInt64LE(b, l)           ((int64_t)getBufferWordLE(b, l, 8))
-double getBufferDouble(void* buffer, int loc);
-char* getBufferString(void* buffer, int loc, int len);
-
+#define getBufferByte(b, l) ((int64_t)getBufferWordLE(b, l, 1))
+#define getBufferInt16LE(b, l) ((int64_t)getBufferWordLE(b, l, 2))
+#define getBufferInt32LE(b, l) ((int64_t)getBufferWordLE(b, l, 4))
+#define getBufferInt64LE(b, l) ((int64_t)getBufferWordLE(b, l, 8))
+double getBufferDouble(void *buffer, int loc);
+char *getBufferString(void *buffer, int loc, int len);
