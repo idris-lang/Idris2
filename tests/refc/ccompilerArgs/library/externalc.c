98026cc3e471b2006ec81eb45aa71fcd35b5e16b
Adam Brouwers-Harries <abrouwersharries@mozilla.com>
2023-12-12 21:40:06 +0000

    [test] Correctly clang-format test inputs

--- tests/refc/ccompilerArgs/library/externalc.c
+++ tests/refc/ccompilerArgs/library/externalc.c
@@ -1,20 +1,17 @@
 #include "externalc.h"
 
-int add(int x, int y) {
-    return x+y;
-}
+int add(int x, int y) { return x + y; }
 
 int fastfibsum(int x) {
-    int acc = 0;
-    int p = 0;
-    int c = 1;
-    int tmp;
-    for (;0 <=-- x;)
-    {
-        acc += c;
-        tmp = c;
-        c = c + p;
-        p = tmp;
-    }
-    return acc;
+  int acc = 0;
+  int p = 0;
+  int c = 1;
+  int tmp;
+  for (; 0 <= --x;) {
+    acc += c;
+    tmp = c;
+    c = c + p;
+    p = tmp;
+  }
+  return acc;
 }
