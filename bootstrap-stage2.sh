1eb5d258c95dcfac18c773e560f8c4636ae3776d
Mathew Polzin <matt.polzin@gmail.com>
2023-12-20 15:15:47 -0600

    reduce repetition of specifying library paths in bootstrap stage 2 and at the same time support overriding default bootstrap library locations

--- bootstrap-stage2.sh
+++ bootstrap-stage2.sh
@@ -8,16 +8,18 @@ IDRIS2_CG="${IDRIS2_CG-"chez"}"
 
 # BOOTSTRAP_PREFIX must be the "clean" build root, without cygpath -m
 # Otherwise, we get 'git: Bad address'
-echo "$BOOTSTRAP_PREFIX"
-DYLIB_PATH="$BOOTSTRAP_PREFIX/lib"
+echo "bootstrapping in: $BOOTSTRAP_PREFIX"
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:-"${BOOTSTRAP_PREFIX}/lib"}"
+export DYLD_LIBRARY_PATH="${DYLD_LIBRARY_PATH:-"${BOOTSTRAP_PREFIX}/lib"}"
+export IDRIS2_DATA="${IDRIS2_DATA:-"${BOOTSTRAP_PREFIX}/support"}"
 
-$MAKE bootstrap-libs IDRIS2_CG="$IDRIS2_CG" LD_LIBRARY_PATH="$DYLIB_PATH" \
+$MAKE bootstrap-libs IDRIS2_CG="$IDRIS2_CG" \
     PREFIX="$BOOTSTRAP_PREFIX" SCHEME="$SCHEME"
-$MAKE bootstrap-install IDRIS2_CG="$IDRIS2_CG" LD_LIBRARY_PATH="$DYLIB_PATH" \
+$MAKE bootstrap-install IDRIS2_CG="$IDRIS2_CG" \
     PREFIX="$BOOTSTRAP_PREFIX" SCHEME="$SCHEME"
 
 # Now rebuild everything properly
 $MAKE clean-libs IDRIS2_BOOT="$BOOTSTRAP_PREFIX/bin/idris2"
 $MAKE all IDRIS2_BOOT="$BOOTSTRAP_PREFIX/bin/idris2" IDRIS2_CG="$IDRIS2_CG" \
-    LD_LIBRARY_PATH="$DYLIB_PATH" \
     SCHEME="$SCHEME"
+echo 'bootstrap stage 2 complete'
