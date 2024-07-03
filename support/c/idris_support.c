1931509c925957cb0eee70bd886e2558c0acb43a
Sergey Fedorov <vital.had@gmail.com>
2024-07-04 01:04:22 +0800

    idris_support: fix environ for macOS (#3324)
    * idris_support: fix environ for macOS
    
    * Add test that touches the new implementation of environ
    
    ---------
    
    Co-authored-by: Sergey Fedorov <barracuda@macos-powerpc.org>
    Co-authored-by: Mathew Polzin <matt.polzin@gmail.com>

--- support/c/idris_support.c
+++ support/c/idris_support.c
@@ -23,6 +23,9 @@ char **_argv;
 extern char **_environ;
 #include "windows/win_utils.h"
 #define environ _environ
+#elif defined(__APPLE__)
+#include <crt_externs.h>
+#define environ (*_NSGetEnviron())
 #else
 extern char **environ;
 #endif
