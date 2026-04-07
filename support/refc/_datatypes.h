9cb06e2cc66358f1dd8d7a5158eb234f546e3ddc
Joel Berkeley <16429957+joelberkeley@users.noreply.github.com>
2026-04-07 02:20:12 +0100

    fmt

--- support/refc/_datatypes.h
+++ support/refc/_datatypes.h
@@ -60,7 +60,7 @@ them first. Of course, this flag is not used if it is clear that Idris2_Value*
 is actually an Int. But places like newReference/removeReference require this
 flag.
  */
-#define idris2_vp_is_unboxed(p) ((uintptr_t)(p) & 3)
+#define idris2_vp_is_unboxed(p) ((uintptr_t)(p)&3)
 
 #define idris2_vp_int_shift                                                    \
   ((sizeof(uintptr_t) >= 8 && sizeof(Idris2_Value *) >= 8) ? 32 : 16)
